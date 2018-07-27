2fec4038443c63b92c734daffc50747a570d7809
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-27 13:55:11 -0400

    Done.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :person
+
+  def address_attributes=(address)
+    self.address = Address.find_or_create_by(name: address.name)
+    self.address.update(address)
+  end
 end
